cfd6c812a51cc027bb80152aca18eb1923bb2d86
GzzhM <979218087@qq.com>
2023-05-16 12:30:46 +0800

    Gzm, test: 增加了geecache的测试

--- geecache_test.go
+++ geecache_test.go
@@ -1,14 +1,16 @@
 package geecache
 
 import (
+	"fmt"
+	"log"
 	"reflect"
 	"testing"
 )
 
 var db = map[string]string{
-	"Tom": "630",
-	"Jac": "589",
-	"Sam": "567",
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
 }
 
 func TestGetter(t *testing.T) {
@@ -20,5 +22,33 @@ func TestGetter(t *testing.T) {
 
 	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
 		t.Errorf("callback failed")
+	}
+}
+func TestGet(t *testing.T) {
+	loadCounts := make(map[string]int, len(db))
+	gee := NewGroup("scores", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				if _, ok := loadCounts[key]; !ok {
+					loadCounts[key] = 0
+				}
+				loadCounts[key] += 1
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	for k, v := range db {
+		if view, err := gee.Get(k); err != nil || view.String() != v {
+			t.Fatal("failed to get value of Tom")
+		}
+		if _, err := gee.Get(k); err != nil || loadCounts[k] > 1 {
+			t.Fatalf("cache %s miss", k)
+		}
+	}
+
+	if view, err := gee.Get("unknown"); err == nil {
+		t.Fatalf("the value of unknow should be empty,bust %s got", view)
 	}
 }
